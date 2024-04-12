6cc0f62a73d003bcbb0803aff5828aa0335e5d15
Jeff Hale <jeffmshale@gmail.com>
2024-04-12 14:10:36 -0400

    uu

--- env-ex.py
+++ env-ex.py
@@ -4,7 +4,7 @@ from prefect import flow
 
 @flow(log_prints=True)
 def some_work():
-    not_so_secret_value = "x"  # os.environ.get("wow")
+    not_so_secret_value = os.environ.get("yo")
     print(not_so_secret_value)
 
 
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         entrypoint="env-ex.py:some_work",
     ).deploy(
         name="my-dep",
-        work_pool_name="dock1",
+        work_pool_name="p1",
     )
 
 
