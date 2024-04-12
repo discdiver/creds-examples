32d075e76e7b2b68e7fdf8ae760b7fa881c9aebb
Jeff Hale <jeffmshale@gmail.com>
2024-04-12 13:21:52 -0400

    source

--- env-ex.py
+++ env-ex.py
@@ -8,7 +8,10 @@ def some_work():
     print(not_so_secret_value)
 
 
-some_work.from_source(source="", entrypoint="").deploy(
+some_work.from_source(
+    source="https://github.com/discdiver/creds-examples.git",
+    entrypoint="env-ex.py:some_work",
+).deploy(
     name="my-dep",
     work_pool_name="p1",
     job_variables={"VAR1": {os.environ.get("yo")}},
